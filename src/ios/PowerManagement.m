7633591ec1acdbb4d13e74439f59f751b4753267
remosti <remosti@hotmail.com>
2022-09-06 14:13:29 +0200

    Update PowerManagement.m
    - Implemented the Fix for Issue: https://github.com/Viras-/cordova-plugin-powermanagement/issues/30

--- src/ios/PowerManagement.m
+++ src/ios/PowerManagement.m
@@ -1,12 +1,12 @@
 /*
  * Copyright 2013 Wolfgang Koller
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,40 +25,29 @@
 @implementation PowerManagement
 - (void) acquire:(CDVInvokedUrlCommand*)command
 {
-    CDVPluginResult* result = nil;
-    
     // Acquire a reference to the local UIApplication singleton
     UIApplication* app = [UIApplication sharedApplication];
-    
+
+    CDVPluginResult *pluginResult = nil;
     if( ![app isIdleTimerDisabled] ) {
         [app setIdleTimerDisabled:true];
-        
-        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
     }
-    else {
-        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ILLEGAL_ACCESS_EXCEPTION messageAsString:@"IdleTimer already disabled"];
-    }
-    
-	[self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 
 - (void) release:(CDVInvokedUrlCommand*)command
-{    
-    CDVPluginResult* result = nil;
-    
+{
     // Acquire a reference to the local UIApplication singleton
     UIApplication* app = [UIApplication sharedApplication];
     
+    
+    CDVPluginResult *pluginResult = nil;
     if( [app isIdleTimerDisabled] ) {
         [app setIdleTimerDisabled:false];
-        
-        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
     }
-    else {
-        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ILLEGAL_ACCESS_EXCEPTION messageAsString:@"IdleTimer not disabled"];
-    }
-    
-	[self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 @end
